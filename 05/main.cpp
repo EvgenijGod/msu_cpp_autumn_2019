517a7bad13c82dde7f0fe497396c3dc38e81f7d4
EvgenijGod <EvgenijGod@users.noreply.github.com>
2019-11-14 09:54:41 +0300

    05

--- 05/main.cpp
+++ 05/main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sstream>
 #include <limits.h>
+#include <assert.h>
 #include "serializer.h"
 using namespace std;
 
@@ -16,26 +17,44 @@ struct Data
     {
         return serializer(a, b, c);
     }
+    template <class Deserializer>
+    Error deserialize(Deserializer& deserializer)
+    {
+        return deserializer(a, b, c);
+    }
+
 };
 
 int main() {
-    Data x { 1, true, 2 };
-
-    std::stringstream stream;
-
+    Data x { 1, false, 2 };
+    stringstream stream;
     Serializer serializer(stream);
     serializer.save(x);
-    cout << stream.str();
-    return 0;
+    cout << stream.str() << endl;
+
     Data y { 0, false, 0 };
 
-    /*Deserializer deserializer(stream);
+    Deserializer deserializer(stream);
     const Error err = deserializer.load(y);
-
     assert(err == Error::NoError);
-
     assert(x.a == y.a);
     assert(x.b == y.b);
-    assert(x.c == y.c);*/
+    assert(x.c == y.c);
+
+    Data x1 { 42, false, 207 };
+    stringstream stream1;
+    Serializer serializer1(stream1);
+    serializer1.save(x1);
+    cout << stream1.str() << endl;
+
+    Data y1 { 42, true, 77};
+
+    Deserializer deserializer1(stream1);
+    const Error err1 = deserializer1.load(y1);
+    assert(err == Error::NoError);
+    assert(x1.a == y1.a);
+    assert(x1.b == y1.b);
+    assert(x1.c == y1.c);
+    cout << "OK";
     return 0;
 }
--- 05/serializer.h
+++ 05/serializer.h
@@ -2,7 +2,6 @@
 #define SPHERE_MATRIX_H
 
 #include <iostream>
-
 #pragma once
 
 #include <ostream>
@@ -42,53 +41,79 @@ class Serializer
 public:
     explicit Serializer(std::ostream& out) : out_(out) {}
     template <class T>
-    Error save(T& object)
+    Error save(T&& object)
     {
         return object.serialize(*this);
     }
     template <class... ArgsT>
-    Error operator()(ArgsT... args)
+    Error operator()(ArgsT&&... args)
     {
-        return process(args...);
+        return process(std::forward<ArgsT>(args)...);
     }
 };
 
 class Deserializer
-{   private:
+{
+private:
     std::istream &in_;
     static constexpr char Separator = ' ';
-    void Save(bool val) {
-        if (val == true)
-            out_ << "true" << Separator;
-        else
-            out_ << "false" << Separator;
-    }
-    void Save(uint64_t val) {
-        out_ << val << Separator;
-    }
+    std::string tmp;
+
     template <class T>
     Error process(T&& val)
     {
-        load(val);
-        return Error::NoError;
+        return ser_load(val);
     }
+
     template <class T, class... Args>
     Error process(T&& val, Args&&... args)
     {
-        load(val);
-        return process(std::forward<Args>(args)...);
+        if(Error::CorruptedArchive != ser_load(val)) {
+            return process(std::forward<Args>(args)...);
+        } else {
+            return Error::CorruptedArchive;
+        }
+    }
+
+    Error ser_load(uint64_t &val) {
+        in_ >> tmp;
+        try {
+            if (tmp[0] <= '9' && tmp[0] >= '0') {
+                val = strtoull(tmp.c_str(), NULL ,10);
+                return Error::NoError;
+            } else {
+                return Error::CorruptedArchive;
+            }
+        } catch (const std::exception &e) {
+            return Error::CorruptedArchive;
+        }
+    }
+    Error ser_load(bool &val) {
+        in_ >> tmp;
+        if (!(tmp == "true" || tmp == "false")) {
+            return Error::CorruptedArchive;
+        } else {
+            if (tmp == "true") {
+                val = true;
+            } else {
+                val = false;
+            }
+            return Error::NoError;
+        }
     }
 public:
     explicit Deserializer(std::istream& in) : in_(in) {}
+
     template <class T>
-    Error save(T& object)
+    Error load(T& object)
     {
-        return object.serialize(*this);
+        return object.deserialize(*this);
     }
+
     template <class... ArgsT>
-    Error operator()(ArgsT... args)
+    Error operator()(ArgsT&&... args)
     {
-        return process(args...);
+        return process(std::forward<ArgsT>(args)...);
     }
 };
 #endif //SPHERE_MATRIX_H
